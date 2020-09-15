23bbb828af2ebf12338b4a59d702539b8834e15e
Eilish McMaster <eilish.summer@gmail.com>
2020-09-15 12:43:43 +1000

    Removes alignments where other contig aligns to the start of two ref contigs or the end of two ref contigs

--- kea_modular/remove_duplicates.py
+++ kea_modular/remove_duplicates.py
@@ -1,3 +1,4 @@
+import numpy as np
 def remove_dupes(filtered_nucmer):
     # make a pivot table with the number of duplicates in the ref_contig column
     dupes = filtered_nucmer.pivot_table(index=['ref_contig'], aggfunc='size')
@@ -33,5 +34,39 @@ def remove_dupes(filtered_nucmer):
     filtered_nucmer = filtered_nucmer[~filtered_nucmer['other_contig'].isin(contigs_to_remove2)]
     #sort so that the dictionary in the next function will work right
     filtered_nucmer = filtered_nucmer.sort_values('ref_start', ascending=False)
+
+    print(filtered_nucmer)
+    start_end_dict = {}
+    for index, alignment in filtered_nucmer.iterrows():
+        if int(alignment['mag_start'])>=200:
+            start_end_dict.update({str(alignment['ref_contig']):[0,str(alignment['other_contig'])]}) #match is start of ref contig =0
+        else:
+            start_end_dict.update({str(alignment['ref_contig']):[1,str(alignment['other_contig'])]}) #match is at end of ref contig = 1
+
+    print('start end dict: ', start_end_dict)
+
+    # make dictionary that has {'[1, other contig]':[ref1, ref2]}
+    flipped = {}
+    for key, value in start_end_dict.items():
+        if str(value) not in flipped:
+            flipped[str(value)] = [key]
+        else:
+            flipped[str(value)].append(key)
+
+    length_dict = {key: len(value) for key, value in flipped.items()}
+    other_remove_list = []  # list of other contig things to remove
+    ref_remove_list = []  # list of reference contigs to remove
+    # find other contig amtches and extract
+    for key, value in length_dict.items():
+        if value >= 2:
+            other_remove_list.append(key[5:-2])
+    # make a list of the reference contigs to remove from array
+    for item in other_remove_list:
+        for key, value in start_end_dict.items():
+            if item == value[1]:
+                ref_remove_list.append(key)
+
+    filtered_nucmer = filtered_nucmer[~filtered_nucmer['ref_contig'].isin(ref_remove_list)]
+    print(filtered_nucmer)
 
     return filtered_nucmer
