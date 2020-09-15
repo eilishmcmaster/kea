a37bc3677c21de6a808d28bca5e6adb4a8b8d08e
Eilish McMaster <eilish.summer@gmail.com>
2020-09-15 13:18:55 +1000

    Added somthing in align scenario 2 to try deal with c1s2<c1s1 (not working yet)

--- kea_modular/align_scenarios.py
+++ kea_modular/align_scenarios.py
@@ -72,30 +72,73 @@ def scenario_2(other_input, ref_input, c1s1,c1e1,c1s2,c1e2,c3s,c3e,c2s,c2e,key,d
     contig1 = ''
     contig2 = ''
     contig3 = ''
-    for other_fasta in SeqIO.parse(other_input, 'fasta'):
-        if other_fasta.id == key:
-            print(datetime.now(), 'Closing gap using other contig ', key)
-            c1 = other_fasta.seq  # sequence of the key 'other_contig'
-            c1_1 = c1[c1s1:c1s2]  # section of contig aligning with ref 1
-            c1_2 = c1[c1s2:c1e1]  # section of contig aligning with ref 2
-            c1_3 = c1[c1e1:c1e2]  # unused section of contig 1
-            contig3 = key
 
-    for rep_fasta in SeqIO.parse(ref_input, 'fasta'):
-        if rep_fasta.id == dict.get(key)[1]:
-            print(datetime.now(), 'Reference contig 1: ', dict.get(key)[1])
-            c3 = rep_fasta.seq  # sequence of first 'rep_contig' in value list
-            c3_1 = c3[c3s:(c3s+c1e1-c1s2)]  # section aligning with 'other' contig
-            c3_2 = c3[(c3s+c1e1-c1s2):c3e]  # unused section of contig 3
-            u2 = c3[c3s:]
-            contig2 = dict.get(key)[1]
-        if rep_fasta.id == dict.get(key)[0]:
-            print(datetime.now(), 'Reference contig 2: ', dict.get(key)[0])
-            c2 = rep_fasta.seq  # sequence of first 'rep_contig' in value list
-            c2_1 = c2[c2s:(c2s+c1s2-c1s1)]  # section aligning with 'other' contig
-            c2_2 = c2[(c2s+c1s2-c1s1):c2e]  # unused part of contig 2
-            u1 = c2[:c2s]
-            contig1 = dict.get(key)[0]
+    # swap_s1=c1s1
+    # swap_s2=c1s2
+    # swap_e1=c1e1
+    # swap_e2=c1e2
+    # c1s1=''
+    # c1e1=''
+    # c1s2=''
+    # c1e2=''
+    #
+    # c1s1=swap_s2
+    # c1s2=swap_s1
+    # c1e1=swap_e2
+    # c1e2=swap_e1
+    if c1s2>c1s1:
+        for other_fasta in SeqIO.parse(other_input, 'fasta'):
+            if other_fasta.id == key:
+                print(datetime.now(), 'Closing gap using other contig ', key)
+                c1 = other_fasta.seq  # sequence of the key 'other_contig'
+                c1_1 = c1[c1s1:c1s2]  # section of contig aligning with ref 1
+                c1_2 = c1[c1s2:c1e1]  # section of contig aligning with ref 2
+                c1_3 = c1[c1e1:c1e2]  # unused section of contig 1
+                contig3 = key
+
+        for rep_fasta in SeqIO.parse(ref_input, 'fasta'):
+            if rep_fasta.id == dict.get(key)[1]:
+                print(datetime.now(), 'Reference contig 1: ', dict.get(key)[1])
+                c3 = rep_fasta.seq  # sequence of first 'rep_contig' in value list
+                c3_1 = c3[c3s:(c3s+c1e1-c1s2)]  # section aligning with 'other' contig
+                c3_2 = c3[(c3s+c1e1-c1s2):c3e]  # unused section of contig 3
+                u2 = c3[c3s:]
+                contig2 = dict.get(key)[1]
+            if rep_fasta.id == dict.get(key)[0]:
+                print(datetime.now(), 'Reference contig 2: ', dict.get(key)[0])
+                c2 = rep_fasta.seq  # sequence of first 'rep_contig' in value list
+                c2_1 = c2[c2s:(c2s+c1s2-c1s1)]  # section aligning with 'other' contig
+                c2_2 = c2[(c2s+c1s2-c1s1):c2e]  # unused part of contig 2
+                u1 = c2[:c2s]
+                contig1 = dict.get(key)[0]
+    else:
+        start2=c1s1
+        start1=c1s2
+        for other_fasta in SeqIO.parse(other_input, 'fasta'):
+            if other_fasta.id == key:
+                print(datetime.now(), 'Closing gap using other contig ', key)
+                c1 = other_fasta.seq  # sequence of the key 'other_contig'
+                c1_1 = c1[start2:start1]  # section of contig aligning with ref 1
+                c1_2 = c1[start1:c1e1]  # section of contig aligning with ref 2
+                c1_3 = c1[c1e1:c1e2]  # unused section of contig 1
+                contig3 = key
+
+        for rep_fasta in SeqIO.parse(ref_input, 'fasta'):
+            if rep_fasta.id == dict.get(key)[1]:
+                print(datetime.now(), 'Reference contig 1: ', dict.get(key)[1])
+                c3 = rep_fasta.seq  # sequence of first 'rep_contig' in value list
+                c3_1 = c3[c3s:(c3s+c1e1-start1)]  # section aligning with 'other' contig
+                c3_2 = c3[(c3s+c1e1-start1):c3e]  # unused section of contig 3
+                u2 = c3[c3s:]
+                contig2 = dict.get(key)[1]
+            if rep_fasta.id == dict.get(key)[0]:
+                print(datetime.now(), 'Reference contig 2: ', dict.get(key)[0])
+                c2 = rep_fasta.seq  # sequence of first 'rep_contig' in value list
+                c2_1 = c2[c2s:(c2s+start1-start2)]  # section aligning with 'other' contig
+                c2_2 = c2[(c2s+start1-start2):c2e]  # unused part of contig 2
+                u1 = c2[:c2s]
+                contig1 = dict.get(key)[0]
+
 
     consensus_1 = ''
     for i in range(len(c2_1)):
